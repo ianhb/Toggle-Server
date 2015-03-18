b3441c0239cf4cc33a414dc6e2ed8e3cdad46bb4
Ian Blakley <ian@810labs.me>
2015-03-18 15:53:01 -0400

    Toggle Server Version 2
    Added an online update checker
    
    Tested with Toggle 1.0.1

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,11 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URI;
 import java.net.URL;
 
 /**
@@ -12,8 +16,11 @@ import java.net.URL;
  */
 public class Main {
 
+    final public static int VERSION = 2;
+
     public static void main(String[] args) {
 
+        checkVersion();
         SettingsWindow window = new SettingsWindow();
         setupMenu(window);
         while (true) {
@@ -67,6 +74,43 @@ public class Main {
             } catch (Exception e) {
                 System.out.println("Couldn't create menu");
             }
+        }
+    }
+
+    private static void checkVersion() {
+        try {
+            final URI toggleSite = new URI("http://810labs.me/toggle");
+            URL url = new URL("http://810labs.me/toggle/version_check");
+            BufferedReader in = new BufferedReader(new InputStreamReader(url.openStream()));
+            int currentVersion = Integer.parseInt(in.readLine());
+            if (currentVersion > VERSION) {
+                JFrame frame = new JFrame("New Version Available");
+                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                frame.setLocationRelativeTo(null);
+                frame.setLayout(new FlowLayout());
+                JLabel label = new JLabel("Toggle Server Version " + currentVersion + " is available");
+                JButton button = new JButton();
+                button.setText("Download Here");
+                button.addActionListener(new ActionListener() {
+                    @Override
+                    public void actionPerformed(ActionEvent e) {
+                        if (Desktop.isDesktopSupported()) {
+                            try {
+                                Desktop.getDesktop().browse(toggleSite);
+                            } catch (IOException z) {
+                                z.printStackTrace();
+                            }
+                        }
+                        System.exit(0);
+                    }
+                });
+                frame.getContentPane().add(label);
+                frame.getContentPane().add(button);
+                frame.pack();
+                frame.setVisible(true);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 }
